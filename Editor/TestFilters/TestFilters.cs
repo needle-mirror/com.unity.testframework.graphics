bf22cc5a9354e5e5fcddd62b6bf8590447c52fa1
Unity Technologies <@unity>
2021-01-28 00:00:00 +0000

    com.unity.testframework.graphics@7.8.1-preview ## [7.8.1-preview] - 2021-01-28 * Test filter sort now uses stable sorting with additional properties

--- Editor/TestFilters/TestFilters.cs
+++ Editor/TestFilters/TestFilters.cs
@@ -19,11 +19,11 @@ public class TestFilters : ScriptableObject
                 (a, b) => a == null ? 1 : b == null ? -1 : a.name.CompareTo(b.name));
         }
 
-        Array.Sort(filters,
-            (a, b) =>
-            a.FilteredScenes.FirstOrDefault() == null ? 1 :
-            b.FilteredScenes.FirstOrDefault() == null ? -1 :
-            a.FilteredScenes.FirstOrDefault().name.CompareTo(b.FilteredScenes.FirstOrDefault().name));
+        var sortedFilters = filters.OrderBy(s => s.FilteredScenes.FirstOrDefault() == null ? string.Empty : s.FilteredScenes.FirstOrDefault().name)
+            .ThenBy(s => s.ColorSpace)
+            .ThenBy(s => s.BuildPlatform)
+            .ThenBy(s => s.GraphicsDevice).ToArray();
+        Array.Copy(sortedFilters, filters, filters.Length);
     }
 }
 #endif
